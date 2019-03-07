8fda6e4685d667934244353f54273a9a1c661c35
richa <richardmel@outlook.com>
2019-03-07 09:47:46 -0600

    A Hello World ReactJS APP first commit

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  render() {
+    return (
+
+      <div>
+
+        <header className="App-header">
+          {
+          //<img src={logo} className="App-logo" alt="logo" />
+          }
+          <h2>Welcome to React! Richard Kalibbala</h2>
+        </header>
+
+      </div>
+
+    );
+  }
+}
+
+export default Header;
---
+++ src/Intro.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+export default class Intro extends Component {
+  render() {
+    return (
+      <div>
+        <p className="App-intro">
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://kalibbala.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit My Site
+        </a>
+
+      </div>
+    );
+  }
+}
